e61c2c5b26b0eb2bafe94a4bb05ebe2df74b075b
ledat2110 <letuandat2110@gmail.com>
2021-02-03 06:51:49 +0700

    restructure files and folders

---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+setup(name = 'src',
+        version = '0.1.0',
+        packages = ['src', 'src/gym_sp'])
--- unit_test/testEnv.py
+++ unit_test/testEnv.py
@@ -2,7 +2,7 @@ from src.gym_sp.supply_chain import *
 import json
 
 
-with open('./env_config/envConfig.json', 'r') as f:
+with open('../env_config/envConfig.json', 'r') as f:
     config = json.load(f)
 
 env = SupplyChain(config)
