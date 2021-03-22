0f7a914cb0882a9864e726cc419bba2ed8f3ae8b
ledat2110 <letuandat2110@gmail.com>
2021-03-22 19:52:37 +0700

    run policy gradient method

--- experiments/threshold_method.py
+++ experiments/threshold_method.py
@@ -1,13 +1,17 @@
 import numpy as np
 import gym
+import collections
 
-from src.agents.threshold_policy import ThresholdAgent
-from src.gym_sp.supply_chain import SupplyChain
+import drl_lib
+from drl_lib.env.supply_chain import SupplyChain
 
 from tensorboardX import SummaryWriter
 
 TEST_EPISODES = 3000
 ENV_CONFIG_PATH = "../env_config/envConfig.json"
+
+Result = collections.namedtuple("Result", field_names=['reward', 'step', 'done'])
+
 
 def create_agent (env: SupplyChain):
     action_dim = env.action_space.shape[0]
@@ -15,11 +19,11 @@ def create_agent (env: SupplyChain):
     eps = env.storage_capacity.copy() / 10
     Q = env.storage_capacity.copy()
 
-    agent = ThresholdAgent(tuple(eps), tuple(Q), action_dim)
+    agent = drl_lib.agent.ThresholdAgent(tuple(eps), tuple(Q), action_dim)
 
     return agent
 
-def simulate_episode (env: SupplyChain, agent: ThresholdAgent):
+def simulate_episode (env: SupplyChain, agent):
     state = env.reset()
     num_storages = env.num_stores
     total_reward = 0
@@ -31,30 +35,32 @@ def simulate_episode (env: SupplyChain, agent: ThresholdAgent):
 
         total_reward += reward
         steps += 1
-        if done:
-            break 
         state = new_state
-
-    mean_reward = total_reward / steps
-    return total_reward, mean_reward
+        yield Result(total_reward, steps, done)
+        if done:
+            env.reset()
+            total_reward = 0
+            steps = 0
 
 if __name__ == "__main__":
-    env = SupplyChain(num_period=25, store_cost=np.array([0, 2, 2, 2]), truck_cost=np.array([3, 3, 3]), price=7)
+    #env = SupplyChain(num_period=25, store_cost=np.array([0, 2, 2, 2]), truck_cost=np.array([3, 3, 3]), price=7)
+    env = SupplyChain(periodic_demand=False)
     agent = create_agent(env)
     writer = SummaryWriter(comment="-threshold_policy-v0")
     iter_no = 0
+    total_reward = []
+    done_episode = 0
 
-    while True:
-        iter_no += 1
-        reward, reward_m = simulate_episode(env, agent)
+    with drl_lib.tracker.RewardTracker(writer, 100) as tracker:
+        for step_idx, result in enumerate(simulate_episode(env, agent)):
+            reward, step, done = result.reward, result.step, result.done
+            if done:
+                tracker.update(reward, step_idx)
+                done_episode += 1
 
-        print("%d: total_reward %.3f, mean reward %.3f"%(iter_no, reward, reward_m))
-        writer.add_scalar("reward", reward, iter_no)
-        writer.add_scalar("mean_reward", reward_m, iter_no)
-
-        if iter_no >= TEST_EPISODES:
-            break
-    
+            if done_episode > TEST_EPISODES:
+                break
+        
     writer.close()
     
     
